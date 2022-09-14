5c0816dd1f11d08f0e71c113078fa43a2ba9b669
andrey <andreycauam@gmail.com>
2022-09-14 18:44:50 -0300

    new route, show notes

--- src/controllers/movie_notesController.js
+++ src/controllers/movie_notesController.js
@@ -8,9 +8,9 @@ class movies_notesController {
 
     const { user_id } = request.params
 
-    if (rating < 0 && rating > 5) {
-      throw new AppError('O rating deve ser um valor entre o número 0 e 5')
-    }
+    // if (rating < 0 && rating > 5) {
+    //   throw new AppError('O rating deve ser um valor entre o número 0 e 5')
+    // }
 
     const note_id = await knex('movies_notes').insert({
       title,
@@ -30,6 +30,19 @@ class movies_notesController {
     await knex('movie_tags').insert(tagsInsert)
 
     response.json()
+  }
+
+  async show(request, response) {
+    const { id } = request.params
+
+    const note = await knex('movies_notes').where({ id }).first()
+
+    const tags = await knex('movie_tags').where({ note_id: id }).orderBy('name')
+
+    return response.json({
+      ...note,
+      tags
+    })
   }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -5,5 +5,6 @@ const Movie_NotesController = require('../controllers/movie_notesController')
 const movie_NotesController = new Movie_NotesController()
 
 notesRoutes.post('/:user_id', movie_NotesController.create)
+notesRoutes.get('/:id', movie_NotesController.show)
 
 module.exports = notesRoutes
